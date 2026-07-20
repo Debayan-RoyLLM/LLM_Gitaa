5fe78aac60e7705d751cd536173ae56628724ea1
Debayan-RoyLLM <debayan@gitaa.in>
2026-07-20 17:22:31 +0530

    docker files

--- start-llm.sh
+++
@@ -1,9 +0,0 @@
-\MODEL_NAME=qwen-2.5-coder-7b \
-MODEL_PATH=/mnt/f_disk/gitaa/debayan/Internal_LLM/models/Qwen2.5-Coder-7B \
-<<<<<<< HEAD
-GPU_INDEX=1 \
-nohup python3 start_model.py &
-=======
-GPU_INDEX=2 \
-nohup python3 start_model.py &
->>>>>>> 
